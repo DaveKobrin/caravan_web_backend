47cc65b4f5440a1c4c6d6cc79cbb3b6eafd29690
dkobrin <shdwkat999@yahoo.com>
2022-09-21 20:32:14 -0400

    added db schemas

---
+++ models/Users.js
@@ -0,0 +1,19 @@
+const { Schema } = require("mongoose");
+const Card = require("./Cards");
+
+const mongoose = require(mongoose);
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, reqired: true},
+    gamertag: {type: String, required: true, unique: true},
+    email: {type: String, unique: true},
+    password: {type: String, required: true},
+    balance: {type: Number, default: 100, min: 0},
+    friends: [{type: Schema.Types.ObjectId, ref: User}],
+    ownedCards: [{type: Schema.Types.ObjectId, ref: Card}],
+    deck: [{type: Schema.Types.ObjectId, ref: Card}],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
