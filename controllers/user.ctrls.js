14be4268b67823a02ead349192dbd9eaef354e31
dkobrin <shdwkat999@yahoo.com>
2022-09-23 20:27:56 -0400

    added login route

--- controllers/user.ctrls.js
+++ controllers/user.ctrls.js
@@ -9,10 +9,10 @@ const register = async (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, salt);
     const starterSet = {set: 'Standard'};
     req.body.ownedCards = await db.Card.find(starterSet,'_id');
-    const newUser = await db.User.create(req.body);
-    if (newUser) {
-      req.session.currentUser = newUser;
-      return res.status(200).json({ message: 'registered and logged in new user' });
+    const user = await db.User.create(req.body);
+    if (user) {
+      req.session.currentUser = user;
+      return res.status(200).json({ message: 'registered and logged in new user', user });
     }
   } catch (err) {
     //catch any errors
@@ -20,6 +20,24 @@ const register = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+    try {
+        const user = await db.User.findOne({ gamertag: req.body.gamertag });
+        if (!user) return res.status(404).json({ message: 'gamertag not found' });
+        
+        const validLogin = bcrypt.compareSync(req.body.password, user.password);
+        if (validLogin) {
+            req.session.currentUser = user;
+            return res.status(200).json({ message: 'logged in user', user });
+        }
+        return res.status(403).json({ message: 'credentials do not match' });
+    } catch (err) {
+      //catch any errors
+      return res.status(400).json({ error: err.message });
+    }
+  };
+
 module.exports = {
   register,
+  login,
 };
