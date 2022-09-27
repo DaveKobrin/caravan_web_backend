77acf114f04c0224b1090ce1515bf8ff68d18972
dkobrin <shdwkat999@yahoo.com>
2022-09-27 19:11:07 -0400

    pushing to heroku deploy"

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // DEPENDENCIES
 require('dotenv').config();
 const express = require('express');
-// const session = require('express-session');
-// const MongoDBStore = require('connect-mongodb-session')(session);
+// const https = require('https');
+const session = require('express-session');
+// const fs = require('fs');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const cors = require('cors');
 const routes = require('./routes');
 // const cookieParser = require('cookie-parser');
@@ -12,13 +14,16 @@ const routes = require('./routes');
 // const userController = require('./controllers/userController.js');
 
 // CONSTANTS
+// const key = fs.readFileSync('./key.pem');
+// const cert = fs.readFileSync('./cert.pem');
 const app = express();
+// const server = https.createServer({key: key, cert: cert}, app);
 const PORT = process.env.PORT || 3000;
 const REACT_URI = process.env.REACT_URI || 'http://localhost:3001';
 const MONGODB_URI = process.env.MONGODB_URI;
-// const SESSION_SECRET = process.env.SESSION_SECRET;
-// const COOKIE_MAX_AGE = 1000*60*60*24; // login lasts for 24 hours
-const whitelist = [REACT_URI];
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const COOKIE_MAX_AGE = 1000*60*60*1; // login lasts for 1 hour
+const whitelist = [REACT_URI, 'https://localhost:3001'];
 const corsOptions = {
   credentials: true,
   origin: (origin, callback) => {
@@ -30,23 +35,24 @@ const corsOptions = {
 
 // DB CONNECTION
 require('./config/db.connection.js');
-// const mongoDBstore = new MongoDBStore({uri: MONGODB_URI, collection: 'mySessions'});
+const mongoDBstore = new MongoDBStore({uri: MONGODB_URI, collection: 'mySessions'});
 
 // MIDDLEWARE
 app.use(cors(corsOptions));
 // app.use(cookieParser());
-// app.use(session({
-//     secret: SESSION_SECRET,
-//     name: 'session-id',
-//     store: mongoDBstore,
-//     cookie: {
-//         maxAge: COOKIE_MAX_AGE,
-//         sameSite: 'none',
-//         secure: false,
-//     },
-//     resave: false,
-//     saveUninitialized: false,
-// }));
+app.use(session({
+    secret: SESSION_SECRET,
+    name: 'session-id',
+    store: mongoDBstore,
+    cookie: {
+        maxAge: COOKIE_MAX_AGE,
+        // sameSite: 'none',
+        secure: false,
+        httpOnly:false,
+    },
+    resave: true,
+    saveUninitialized: true,
+}));
 app.use(express.static('./public'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/images', express.static(__dirname + '/images'));
@@ -60,6 +66,7 @@ app.use('/cards', routes.card);
 app.use('/user', routes.user);
 
 // START SERVER
+// server
 app.listen(PORT, () => {
   console.log(`Express listening to port : ${PORT}`);
 });
